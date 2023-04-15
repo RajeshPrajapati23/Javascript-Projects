733e3c99283808c5599d3a99f576114f29221586
Rajesh Prajapati <111434481+RajeshPrajapati23@users.noreply.github.com>
2023-04-15 15:46:14 +0530

    Add files via upload

---
+++ word-projects/Quote Generator/script.js
@@ -0,0 +1,19 @@
+
+
+let url = "https://api.quotable.io/random?maxLength=50"
+
+function generateQuote() {
+    try {
+        let content = document.querySelector(".content");
+        let author = document.querySelector(".author");
+        fetch("https://api.quotable.io/random?maxLength=50")
+            .then(res => res.json())
+            .then((data) => {
+                console.log(content.innerHTML = data.content);
+                console.log(author.innerHTML = `By ` + data.author);
+            })
+            .catch()
+    } catch (err) {
+        alert(err.message);
+    }
+}
